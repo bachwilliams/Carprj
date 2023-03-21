46aa75687c67ca41a08f216be6eabff90a56f3be
nguyenhieu1202 <hieunguyenhd2003@gmail.com>
2023-03-21 17:10:17 +0700

    Merge branch 'master' of https://github.com/bachwilliams/Carprj into hieuupdate
    # Conflicts:
    #	build/classes/CarPrj/Classes/Com/CarList.class
    #	nbproject/private/private.xml
    #	src/CarPrj/Classes/Com/Car.java
    #	src/CarPrj/Classes/Com/CarList.java

--- src/CarPrj/Classes/CarManager.java
+++ src/CarPrj/Classes/CarManager.java
@@ -15,86 +15,85 @@ import java.util.Scanner;
 
 public class CarManager {
 
-	public static void main(String[] args) {
-		// Add some cars to the ArrayList
-		//option in menu
-		ArrayList<String> options = new ArrayList<String>();
-		options.add("List all brands");
-		options.add("Add a new brand");
-		options.add("Search a brand based on its ID");
-		options.add("Update a brand");
-		options.add("Save brands to the file, named brands.txt");
-		options.add("List all cars in ascending order of brand names");
-		options.add("List cars based on a part of an input brand name");
-		options.add("Add a car");
-		options.add("Remove a car based on its ID");
-		options.add("Update a car based on its ID");
-		options.add("Save cars to file, named cars.txt");
-		options.add("Exit");
+    public static void main(String[] args) {
+        // Add some cars to the ArrayList
+        //option in menu
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("List all brands");
+        options.add("Add a new brand");
+        options.add("Search a brand based on its ID");
+        options.add("Update a brand");
+        options.add("Save brands to the file, named brands.txt");
+        options.add("List all cars in ascending order of brand names");
+        options.add("List cars based on a part of an input brand name");
+        options.add("Add a car");
+        options.add("Remove a car based on its ID");
+        options.add("Update a car based on its ID");
+        options.add("Save cars to file, named cars.txt");
+        options.add("Exit");
 
-		BrandList blist = new BrandList();
-		String projectPath = System.getProperty("user.dir");
-		System.out.println(projectPath);
-		try{
-			blist.loadFromFile(projectPath + "\\src\\CarPrj\\Brands.txt");
-		}catch(Exception e){}
-		CarList clist = new CarList(blist);
-		try {
-			clist.loadFromFile(projectPath+"\\src\\CarPrj\\Cars.txt");
-			System.out.println("Check 1");
-			System.out.println("Done car");
-		} catch (Exception e) {
-			System.out.println(e);
-			System.out.println("not done car");	
-		}
+        BrandList blist = new BrandList();
+        String projectPath = System.getProperty("user.dir");
+        System.out.println(projectPath);
+        try {
+            blist.loadFromFile(projectPath + "\\src\\CarPrj\\Brands.txt");
+        } catch (Exception e) {
+        }
+        CarList clist = new CarList(blist);
+        try {
+            clist.loadFromFile(projectPath + "\\src\\CarPrj\\Cars.txt");
+            System.out.println("Check 1");
+            System.out.println("Done car");
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("not done car");
+        }
 
-		int choice = 0;
-		Menu menu = new Menu();
-		do {
-			System.out.println("\nCar managing Program");
-			choice = Menu.int_getChoice(options);
-			switch (choice) {
-				case 1:
-					blist.listBrands();
-					break;
-				case 2:
-					blist.addBrand();
-					break;
-				case 3: {
-					Scanner sc = new Scanner(System.in);
-					String ID = sc.nextLine();
-					blist.searchID(ID);
-					break;
-				}
-				case 4:
-					blist.updateBrand();
-					break;
-				case 5: {
-					try {
-						blist.saveToFile(projectPath + "\\src\\CarPrj\\Brands.txt");
-					} catch (Exception e) {
-					}
-				}
-				case 6:
-					clist.listCars();
-					break;
-				case 7:
-					clist.printBasedBrandName();
-					break;
-				case 8:
-					clist.addCar();
-					break;
-				case 9:
-					clist.removeCar();
-					break;
-				case 10:
-					clist.updateCar();
-					break;
-				case 11:
-					clist.saveToFile(projectPath + "\\src\\CarPrj\\Cars.txt");
-					break;
-				default: System.out.println("Bye!"); 
-			}
-		} while (choice > 0  && choice<= 11);
-	}
+        int choice = 0;
+        Menu menu = new Menu();
+        do {
+            System.out.println("\nCar managing Program");
+            choice = Menu.int_getChoice(options);
+            switch (choice) {
+                case 1:
+                    blist.listBrands();
+                    break;
+                case 2:
+                    blist.addBrand();
+                    break;
+                case 3: {
+                    Scanner sc = new Scanner(System.in);
+                    System.out.print("Enter the ID you want to search :");
+                    String ID = sc.nextLine();
+                    blist.searchID1(ID);
+                    break;
+                }
+                case 4:
+                    blist.updateBrand();
+                    break;
+                case 5:
+                    blist.saveToFile(projectPath + "\\src\\CarPrj\\Brands.txt"); break;
+                case 6:
+                    clist.listCars();
+                    break;
+                case 7:
+                    clist.printBasedBrandName();
+                    break;
+                case 8:
+                    clist.addCar();
+                    break;
+                case 9:
+                    clist.removeCar();
+                    break;
+                case 10:
+                    clist.updateCar();
+                    break;
+                case 11:
+                    clist.saveToFile(projectPath + "\\src\\CarPrj\\Cars.txt");
+                    break;
+                default:
+                    System.out.println("Bye!");
+            }
+        } while (choice > 0 && choice <= 11);
+    }
 }
--- src/CarPrj/Classes/Com/Car.java
+++ src/CarPrj/Classes/Com/Car.java
@@ -2,70 +2,78 @@ package CarPrj.Classes.Com;
 
 public class Car {
 
-	private String carID;
-	private Brand brand;
-	private String color;
-	private String frameID;
-	private String engineID;
+    private String carID;
+    private Brand brand;
+    private String color;
+    private String frameID;
+    private String engineID;
 
-	public Car() {
-	}
+    public Car() {
+    }
 
-	public Car(String carID, Brand brand, String color, String frameID, String engineID) {
-		this.carID = carID;
-		this.brand = brand;
-		this.color = color;
-		this.frameID = frameID;
-		this.engineID = engineID;
-	}
+    public Car(String carID, Brand brand, String color, String frameID, String engineID) {
+        this.carID = carID;
+        this.brand = brand;
+        this.color = color;
+        this.frameID = frameID;
+        this.engineID = engineID;
+    }
 
-	public String getCarID() {
-		return carID;
-	}
+    public String getCarID() {
+        return carID;
+    }
 
-	public void setCarID(String carID) {
-		this.carID = carID;
-	}
+    public void setCarID(String carID) {
+        this.carID = carID;
+    }
 
-	public Brand getBrand() {
-		return brand;
-	}
+    public Brand getBrand() {
+        return brand;
+    }
 
-	public void setBrand(Brand brand) {
-		this.brand = brand;
-	}
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 
-	public String getColor() {
-		return color;
-	}
+    public String getColor() {
+        return color;
+    }
 
-	public void setColor(String color) {
-		this.color = color;
-	}
+    public void setColor(String color) {
+        this.color = color;
+    }
 
-	public String getFrameID() {
-		return frameID;
-	}
+    public String getFrameID() {
+        return frameID;
+    }
 
-	public void setFrameID(String frameID) {
-		this.frameID = frameID;
-	}
+    public void setFrameID(String frameID) {
+        this.frameID = frameID;
+    }
 
-	public String getEngineID() {
-		return engineID;
-	}
+    public String getEngineID() {
+        return engineID;
+    }
 
-	public void setEngineID(String engineID) {
-		this.engineID = engineID;
-	}
+    public void setEngineID(String engineID) {
+        this.engineID = engineID;
+    }
 
-	@Override
-	public String toString() {
-		return carID + ", " + brand.getBrandID() + ", " + color + ", " + frameID + ", " + engineID;
-	}
+    public void setUpdatedCar(Brand brand, String color, String frameID, String engineID) {
+        this.brand = brand;
+        this.color = color;
+        this.frameID = frameID;
+        this.engineID = engineID;
+    }
 
-	public String screenString() {
-		return brand.getBrandID() + ", \n, " + carID + ", " + color + ", " + frameID + ", " + engineID;
-	}
+    @Override
+    public String toString() {
+        return carID + ", " + brand.getBrandID() + ", " + color + ", " + frameID + ", " + engineID;
+    }
+
+    public String screenString() {
+        System.out.printf("%6s%9s%9s%9s%9s", carID, brand.getBrandID(), color, frameID, engineID);
+        return "";
+    }
 
 }
